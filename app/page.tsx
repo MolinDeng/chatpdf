50bfc98e6475f60b90760a2ed7a3ee4f7f012408
MolinDeng <zjumorning@gmail.com>
2023-10-09 13:13:55 -0400

    add product preview

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,13 @@ import { UserButton, auth } from '@clerk/nextjs';
 import Link from 'next/link';
 import { ArrowRight, LogIn } from 'lucide-react';
 import FileUpload from '@/components/FileUpload';
-import { checkSubscription } from '@/lib/subscription';
-import SubscriptionButton from '@/components/SubscriptionButton';
+// import { checkSubscription } from '@/lib/subscription';
+// import SubscriptionButton from '@/components/SubscriptionButton';
 import { db } from '@/lib/db';
 import { chats } from '@/lib/db/schema';
 import { eq } from 'drizzle-orm';
+import Image from 'next/image';
+import previewImg from '@/public/preview.jpg';
 
 export default async function Home() {
   const { userId } = auth();
@@ -25,7 +27,9 @@ export default async function Home() {
       <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
         <div className="flex flex-col items-center text-center">
           <div className="flex items-center">
-            <h1 className="mr-3 text-5xl font-semibold">Chat with any PDF</h1>
+            <h1 className="mr-3 lg:text-5xl md:text-4xl text-3xl font-semibold">
+              Chat with any PDF
+            </h1>
             <UserButton afterSignOutUrl="/" />
           </div>
 
@@ -44,7 +48,7 @@ export default async function Home() {
             )}
           </div>
 
-          <p className="max-w-xl mt-1 text-lg text-slate-600">
+          <p className="max-w-xl mt-1 md:text-lg text-base text-slate-600">
             Join millions of students, researchers and professinals to instantly
             answer questions and understand research with AI
           </p>
@@ -53,12 +57,28 @@ export default async function Home() {
             {isAuth ? (
               <FileUpload />
             ) : (
-              <Link href="/sign-in">
-                <Button>
-                  Get Started!
-                  <LogIn className="w-4 h-4 ml-2" />
-                </Button>
-              </Link>
+              <div>
+                <Link href="/sign-in">
+                  <Button>
+                    Get Started!
+                    <LogIn className="w-4 h-4 ml-2" />
+                  </Button>
+                </Link>
+                <div className="mx-auto max-w-6xl px-6 lg:px-8">
+                  <div className="mt-8 flow-root">
+                    <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
+                      <Image
+                        src={previewImg}
+                        alt="product preview"
+                        width={1364}
+                        height={866}
+                        quality={100}
+                        className="rounded-md bg-white shadow-2xl ring-1 ring-gray-900/10"
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
             )}
           </div>
         </div>
